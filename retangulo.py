473d454676e17b2f89e36f6186768fafcb35f53f
davilucas <91565261+Davilucca22@users.noreply.github.com>
2024-01-11 19:52:04 -0300

    Update retangulo.py

--- retangulo.py
+++ retangulo.py
@@ -4,6 +4,6 @@ altura = float(input('digite a altura do retangulo: '))
 area = base * altura
 perimetro = (2*base) + (2*altura)
 diagonal = (base**2 + altura**2)**0.5
-print(f'area do retangulo:{area}')
-print(f'perimetro do retangulo:{perimetro}')
+print(f'area do retangulo:{area:.4f}')
+print(f'perimetro do retangulo:{perimetro:.4f}')
 print(f'diagonal do retangulo:{diagonal:.4f}')
