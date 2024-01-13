4957c84bbb25b04a697a68a8768aa70b212a4415
davilucas <91565261+Davilucca22@users.noreply.github.com>
2024-01-12 21:38:54 -0300

    consumo

---
+++ consumo.py
@@ -0,0 +1,6 @@
+distancia = float(input('distancia percorrida:'))
+gas = float(input('combustivel gasto:'))
+consumo = distancia / gas
+print('-'*30)
+print(f'consumo media:{consumo:.3f}')
+print('-'*30)
